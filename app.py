8884efe3f91632e5bf0ba3b332f763e5ee8d25b0
Ayan Khan <khan4506342@cloud.neduet.edu.pk>
2025-05-10 04:31:21 +0500

    ..

--- app.py
+++ app.py
@@ -172,5 +172,6 @@ def send_all():
         return f"An error occurred: {str(e)}", 500
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
+
